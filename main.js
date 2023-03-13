7de472caedac9a0e8ea5246839cbdb9bb17958bb
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-03-13 15:36:23 +0900

    first

---
+++ main.js
@@ -0,0 +1,30 @@
+document.getElementById('time').innerHTML
+new Date().toLocaleString()
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
+function reset() {
+  document.body.style.backgroundColor = 'white'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/html5.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
+}
+function showcss() {
+  document.getElementById('fig').src = 'img/css3.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
+}
+function showjavascript() {
+  document.getElementById('fig').src = 'img/javascript.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
+}
